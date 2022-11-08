853d7f18e0fdf4f517c260aa479cde43fd6cb76b
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2022-11-08 18:27:16 -0500

    comments

--- views/monsters/Show.jsx
+++ views/monsters/Show.jsx
@@ -11,19 +11,12 @@ class Show extends React.Component {
         {regionalForm ? <span className='cap'>{regionalForm}<br /></span> : ''}
 
         {this.props.pageType === 'type' ? primaryType === category ? <>Primary type: <a className='cap' href={`/pokemon/type/${primaryType}`}>{primaryType}</a><br /></> : secondaryType === category ? <>Secondary type: <a className='cap' href={`/pokemon/type/${secondaryType}`}>{secondaryType}</a><br /></> : '' : <>Primary type: <a className='cap' href={`/pokemon/type/${primaryType}`}>{primaryType}</a><br />{secondaryType ? <>Secondary type: <a className='cap' href={`/pokemon/type/${secondaryType}`}>{secondaryType}</a><br /></> : ''}</>}
-
         {species}<br />
-
         <a className='cap' href={`/pokemon/region/${region}`}>{region}</a><br />
-
         <img src={image} width='100px' /><br />
-
         {entry}<br />
-
         {evolutionType ? <>{evolutionType}<br /></> : ''}
-
         {hasBeenCaught === 'caught' ? <><a className='cap' href='/pokemon/status/caught'>caught</a></> : <><a className='cap' href='/pokemon/status/uncaught'>uncaught</a></>}<br />
-
         <form method='POST' action={`/pokemon/${_id}?_method=DELETE`}><input type='submit' value={`Delete ${name}`} /></form><br />
         <a href={`/pokemon/${name.toLowerCase()}/${_id}/edit`}>edit {name}</a>
       </Layout>
